8b92ea8e772b48ac6e84713b13c6ba33c2538a08
K4Lok <samkalok107@gmail.com>
2023-02-02 20:34:07 +0800

    init project with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ContributionsCalendar from "./components/ContributionsCalendar"
+
+function App() {
+
+  return (
+    <div className="App">
+      <ContributionsCalendar />
+    </div>
+  )
+}
+
+export default App
