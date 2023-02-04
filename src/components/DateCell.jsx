80669584b78290bd8d908960a2728690b9f45b58
K4Lok <samkalok107@gmail.com>
2023-02-04 17:47:19 +0800

    update styles and fonts

--- src/components/DateCell.jsx
+++ src/components/DateCell.jsx
@@ -3,7 +3,7 @@ import './DateCell.css'
 export default function DateCell({ children, name = false, style = {}, border = {}, disabled = false }) {
     return (
         <span className={`cell ${name && 'day'} ${disabled && 'disabled'}`}
-            style={{ ...style, ...border }}
+            style={{ ...style, ...border}}
         >
             {children}
         </span>
